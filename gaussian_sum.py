bc0f17723d7a2fc2378c4312cebc436908883098
ybyygu <ybyygu@79057b7e-371c-0410-bd3e-c5fae8ee3056>
2006-08-30 08:51:36 +0000

    add gaussian_sum.py

---
+++ gaussian_sum.py
@@ -0,0 +1,123 @@
+#! /usr/bin/env python
+# -*- coding: UTF-8 -*-
+#===============================================================================#
+#   DESCRIPTION:  
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#         NOTES:  ---
+#        AUTHOR:  ybyygu 
+#       LICENCE:  GPL version 2 or upper
+#       VERSION:  0.1
+#       CREATED:   
+#      REVISION:  ---
+#===============================================================================#
+import sys
+from sys import stdin, stderr
+from os.path import expanduser, join
+import glob
+
+DefaultWD = expanduser("~/gjf/work")
+DefaultLogs = glob.glob(join(DefaultWD, "*.log"))
+
+def SummaryGassianlogFromFiles(gaussian_log_files):
+    for log in gaussian_log_files:
+        try:
+            flog = open(log, 'r')
+        except IOError:
+            print >>stderr, "Can't open %s to read!" %(gaussian_log_file)
+            continue
+
+        code = walklog(flog)
+        flog.close()
+    return code
+
+def SummaryGaussianlogFromStdin():
+    return walklog(sys.stdin)
+
+def walklog(flog):
+    import re
+    
+    LineLength = 72
+
+    line = flog.readline()
+    
+    freq_count = 0
+    while line:
+        if re.compile(r'^ %').match(line):
+            print ' ' + '='*LineLength
+            while line and re.compile(r'^ %').match(line):
+                print line,
+                line = flog.readline()
+        elif re.compile(r'^ #').match(line):
+            print  ' ' + '-'*LineLength
+            print line,
+            for i in range(3):
+                line = flog.readline()
+                if re.compile(r'^ -').match(line):
+                    break
+                else:
+                    print line,
+            print  ' ' + '='*LineLength
+        elif line.find("Number of steps in this run=") >= 0:
+            print line,
+# print SCF information and the next two lines
+        elif line.find("SCF Done") >= 0:
+            print line,
+            for i in range(2):
+                line = flog.readline()
+                print line,
+            print " " + '-'*LineLength
+        elif line.find("Step number") >= 0:
+            print line,
+        elif line.find("exceeded") >= 0:
+            print line,
+# print the first line of Eigenvalues
+        elif line.find("Eigenvalues ---") >= 0:
+            print line,
+            while line and line.find("Eigenvalues ---") >=0:
+                line = flog.readline()
+# print converged information and the next 5 lines
+        elif line.find("Converged?") >= 0:
+            print " " + '-'*LineLength
+            print line, 
+            for i in range(5):
+                line = flog.readline()
+                print line,
+            print  " " + '-'*LineLength
+        elif line.find("Frequencies --") >= 0:
+            freq_count += 1
+            if freq_count == 1:
+                print line,
+        elif line.find("Zero-point correction=") >= 0:
+            print line,
+        elif line.find("Sum of electronic and zero-point Energies=") >= 0:
+            print line,
+            print " " + '-'*LineLength
+        elif line.find("termination") >= 0:
+            print line,
+        elif line.find("Job cpu time:") >= 0:
+            print line,
+
+        line = flog.readline()
+
+#==========================================================================
+# MAIN PROGRAM
+#==========================================================================
+def main (argv=None):
+    import getopt
+
+    if argv is None:  argv = sys.argv
+    
+# try to read from default gaussian output directory
+    if len(argv) == 1:
+        SummaryGassianlogFromFiles(DefaultLogs)
+# try to read from stdin
+    elif argv[1] == '-':
+        SummaryGaussianlogFromStdin()
+    else:
+        SummaryGassianlogFromFiles(argv[1:])
+if (__name__ == "__main__"):
+    result = main()
+    # Comment the next line to use the debugger
+    sys.exit(result)
