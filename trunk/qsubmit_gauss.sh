f86705b0ab9671e5d488d62e123b6791da3f6dbd
ybyygu <ybyygu@79057b7e-371c-0410-bd3e-c5fae8ee3056>
2006-08-30 09:09:40 +0000

    add qsubmit_gauss.sh

---
+++ trunk/qsubmit_gauss.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+# Written by ybyygu at 2004
+# Last updated at 2006 8/21
+
+#------------------------------------------------------------------------
+GAUSSIAN_VAR=$HOME/gaussian/regvar-g03
+GAUSSIAN_CMD="g03"
+GJF_ROOT=$HOME/gjf
+QUEUE_DIR=$GJF_ROOT/queue
+WORK_DIR=$GJF_ROOT/work
+NORM_DONE_DIR=$GJF_ROOT/norm_done
+ERROR_DONE_DIR=$GJF_ROOT/error_done
+LOG=$GJF_ROOT/qsubmit_gauss.log
+#------------------------------------------------------------------------
+
+# set up gaussian environment
+source $GAUSSIAN_VAR
+
+
+submit()
+{
+    exec 2>/dev/null        # turn off all standard error output
+    
+    # clean scratch before starting
+    if [ -d $GAUSS_SCRDIR ]; then
+        rm -f $GAUSS_SCRDIR/*
+    fi
+
+    local files=$(cd $QUEUE_DIR; ls *.gjf *.com)
+    if [ "$files" == "" ]; then
+        exit 1
+    fi
+
+    # submit gaussian jobs
+    local gauss_input
+    for gauss_input in $files; do
+
+        # convert DOS newlines to unix format, and submit it
+        (
+         [[ -d $WORK_DIR ]] || mkdir -p $WORK_DIR
+         cd $WORK_DIR
+         mv $QUEUE_DIR/$gauss_input $WORK_DIR/
+         cat $gauss_input|tr -d '\r' \
+         |$GAUSSIAN_CMD &> ${gauss_input/%.*/.log}
+        )
+        
+        # wait for a long time
+        local str=$(tail $WORK_DIR/${gauss_input%.*}.log |grep "Normal termination")
+        local dir_name=$(date "+%Y-%m%d-%H%M.XXXXXX")
+
+        if [ "$str" == "" ]; then
+            [[ -d $ERROR_DONE_DIR ]] || mkdir -p $ERROR_DONE_DIR
+            local arch_dir=$(mktemp -d $ERROR_DONE_DIR/$dir_name)
+            ( cd $WORK_DIR; mv "$gauss_input" *.log *.chk $arch_dir/ )
+            echo "--------------------------------------"
+            echo "  (II) informational,  (WW) warning   "
+            echo "--------------------------------------"
+            echo "(WW) Gaussian did not terminate normally "
+            echo "(II) Date: `date +%c` "
+            echo "(II) From: $QUEUE_DIR/$gauss_input"
+            echo "(II) To  : $arch_dir/"
+            echo
+        else
+            [[ -d $NORM_DONE_DIR ]] || mkdir -p $NORM_DONE_DIR
+            local arch_dir=$(mktemp -d $NORM_DONE_DIR/$dir_name)
+            
+            ( cd $WORK_DIR; mv "$gauss_input" *.log *.chk $arch_dir/ )
+            echo "--------------------------------------"
+            echo "  (II) informational,  (WW) warning   "
+            echo "--------------------------------------"
+            echo "(II) Date: `date +%c` "
+            echo "(II) From: $QUEUE_DIR/$gauss_input"
+            echo "(II) To  : $arch_dir/"
+            echo
+        fi
+    done
+
+    #recursive call
+    submit
+}
+
+if [ -d "$QUEUE_DIR" ]; then
+    submit >> $LOG
+else
+    echo -n "Your $QUEUE_DIR doesn't exist, do you want to creat it?(y/n)"
+    read answer
+
+    if [ "$answer" == "y" ]; then
+        mkdir -p $QUEUE_DIR
+        echo "Now you can put something into $QUEUE_DIR, then restart this command."
+    fi
+fi
+
+exit 0
