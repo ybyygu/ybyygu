1d25b8a87058a9dc2df861042b2c1e69654c8ce2
ybyygu <ybyygu@79057b7e-371c-0410-bd3e-c5fae8ee3056>
2006-09-02 02:01:52 +0000

    add more elements into fvib.py and xvib.py, thanks to Zhiji.

--- trunk/fvib.py
+++ trunk/fvib.py
@@ -18,12 +18,13 @@ def usage(progname):
     print '    -s scale: Use this value to scale the displacement. The default value is %s.' % SCALE
     print '    -o output.gjf: Use this option to specify an output file.'
 
-ATOMS = ('H ', 'He',
-          'Li', 'Be', 'B ', 'C ', 'N ', 'O ', 'F ', 'Ne',
-          'Na', 'Mg', 'Al', 'Si', 'P ', 'S ', 'Cl', 'Ar',
-          'K ', 'Ca', 'Ti', 'V ', 'Cr', 'Mn', 'Fe', 'Co',
-          'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 
-          'Kr', 'X ')
+ATOMS = ('H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne', 'Na', 'Mg', 'Al', 'Si', 'P', 'S',
+         'Cl', 'Ar', 'K', 'Ca', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge',
+         'As', 'Se', 'Br', 'Kr', 'Rb', 'Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd',
+         'In', 'Sn', 'Sb', 'Te', 'I', 'Xe', 'Cs', 'Ba', 'La', 'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd',
+         'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg',
+         'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn', 'Fr', 'Ra', 'Ac', 'Th', 'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm',
+         'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr', 'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt')
 
 PROGNAME = sys.argv[0]
 # parse command line options
