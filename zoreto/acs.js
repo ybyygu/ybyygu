5b4a4a39427c75c0119ade4ed9222b7beba5c878
Wenping Guo <ybyygu@gmail.com>
2017-10-17 12:14:06 +0800

    removed unrelated scripts

--- zoreto/acs.js
+++
@@ -1,6 +0,0 @@
-function detectWeb(doc, url) {
-	var re = new RegExp('^http://pubs\.acs\.org/cgi-bin/(article|abstract)\.cgi/', 'i');
-	if(re.test(doc.location.href)) {
-		return "journalArticle";
-	}
-}
