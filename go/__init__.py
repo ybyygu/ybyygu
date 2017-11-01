57506b2088bc71bbc0385f4baf93dd4a1d557059
Wenping Guo <ybyygu@gmail.com>
2017-11-01 15:58:54 +0800

    plan to write a python module named as go

---
+++ go/__init__.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+# [[file:~/Workspace/Programming/chem-utils/chem-utils.note::628179c4-3bb6-4ddc-bfbd-4a35162f1c10][628179c4-3bb6-4ddc-bfbd-4a35162f1c10]]
+#===============================================================================#
+#   DESCRIPTION:  GO: a Graph-based chemical Objects library
+#
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#         NOTES:  ---
+#        AUTHOR:  Wenping Guo <ybyygu@gmail.com>
+#       LICENCE:  GPL version 2 or upper
+#       CREATED:  <2006-08-30 Wed 16:51>
+#       UPDATED:  <2017-11-01 Wed 16:09>
+#===============================================================================#
+# 628179c4-3bb6-4ddc-bfbd-4a35162f1c10 ends here
