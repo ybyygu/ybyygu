40ed1b7be92bc5870aa64240cc54b97294ce0923
Wenping Guo <ybyygu@gmail.com>
2018-02-23 19:37:56 +0800

    rust go: parse positions from xyz stream

---
+++ lib/go/src/lib.rs
@@ -0,0 +1,5 @@
+// [[file:~/Workspace/Programming/chem-utils/chem-utils.note::68b8f3aa-b3f8-43c0-8b4d-c3165b146535][68b8f3aa-b3f8-43c0-8b4d-c3165b146535]]
+extern crate cgmath;
+
+mod neighbors;
+// 68b8f3aa-b3f8-43c0-8b4d-c3165b146535 ends here
